ccf8974b0d0ae2a9b1515aeb861c6e7f13307bf6
Lorena Ávila de Souza Silva <lorenaavila15@outlook.com>
2025-10-25 14:18:01 -0300

    feat: Implementa LeitorDeDados com lógica de limpeza de cabeçalho e tratamento de erros.

--- src/main/java/LeitorDeDados.java
+++ src/main/java/LeitorDeDados.java
@@ -1,9 +1,64 @@
-
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException; // Para tratar erros específicos do CSV
-
 import java.io.FileReader;    // Para abrir o arquivo no sistema
 import java.io.IOException;     // Para tratar erros de Input/Output (E/S)
 import java.util.ArrayList;   // Para criar a lista de amostras
 import java.util.List;        // Para usar a interface List
+
+
+public class LeitorDeDados{
+
+    public List<AmostraPonto> lerAmostras(String caminhoArquivo){
+        
+        List<AmostraPonto> amostras = new ArrayList<>(); 
+        
+        // 'linha' irá armazenar o array de strings de cada linha lida
+        String[] linha;  
+    
+        try (FileReader fileReader = new FileReader(caminhoArquivo);
+
+            CSVReader reader = new CSVReaderBuilder(fileReader).build()){ 
+            boolean lendoMetadados = true;             
+            
+            // Loop para pular o cabeçalho (Metadados # + Linha de Nomes)
+            while (lendoMetadados && (linha = reader.readNext()) != null){
+
+                if (linha[0].startsWith("#")){
+                    continue;
+                    
+                } else{
+                    lendoMetadados = false;
+                }
+            }
+            
+            while ((linha = reader.readNext()) != null){
+                
+                try{
+                    // 1. EXTRAIR OS DADOS e converter para double
+                    double lat = Double.parseDouble(linha[44]);
+                    
+                    // TODO: Implementar a Longitude (índice 46)
+                    double lon = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX; 
+                    
+                    // TODO: Implementar a Densidade (índice 193)
+                    double densidade = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX;
+                    
+                    // 2. CRIAR O OBJETO E ARMAZENAR
+                    AmostraPonto amostra = new AmostraPonto(lat, lon, densidade);
+                    amostras.add(amostra);
+
+                } catch (NumberFormatException e){
+                    // Se a linha tem um dado que não é número, apenas ignora
+                }
+            }
+                
+    
+        } catch (IOException | CsvException e){ 
+            // Corrigido para IOException
+            System.err.println("Erro ao ler o arquivo CSV: " + e.getMessage());
+        }
+        
+        return amostras;
+    }
+}
